8fe153177510fd4eb59b5d0e64d2974b4b46a047
Sadhguru <ramdassaj@gmail.com>
2020-04-13 12:51:12 -0400

    update bg and color scheme for level three

--- src/Card.js
+++ src/Card.js
@@ -60,22 +60,22 @@ class Card extends React.Component {
         return(
             <div>
                 <div>
-                    <span className="bg-gold dib br3 pa3 ma2 grow bw4 shadow-5">{this.state.posOne}</span>
-                    <span className="bg-gold dib br3 pa3 ma2 grow bw4 shadow-5">{this.state.posTwo}</span>
-                    <span className="bg-gold dib br3 pa3 ma2 grow bw4 shadow-5">{this.state.posThree}</span>
-                    <span className="bg-gold dib br3 pa3 ma2 grow bw4 shadow-5">{this.state.posFour}</span>
+                    <span className="bg-orange dib br3 pa3 ma2 grow bw4 shadow-5">{this.state.posOne}</span>
+                    <span className="bg-orange dib br3 pa3 ma2 grow bw4 shadow-5">{this.state.posTwo}</span>
+                    <span className="bg-orange dib br3 pa3 ma2 grow bw4 shadow-5">{this.state.posThree}</span>
+                    <span className="bg-orange dib br3 pa3 ma2 grow bw4 shadow-5">{this.state.posFour}</span>
                 </div>
                 <br />
                 <form>
-                    <input type="text" maxLength="2" className="bg-gold dib br3 pa3 ma2 grow bw2 shadow-5" id="1" onChange={this.handleInputChange}></input>
-                    <input type="text" maxLength="2" className="bg-gold dib br3 pa3 ma2 grow bw2 shadow-5" id="2" onChange={this.handleInputChange}></input>
-                    <input type="text" maxLength="2" className="bg-gold dib br3 pa3 ma2 grow bw2 shadow-5" id="3" onChange={this.handleInputChange}></input>
-                    <input type="text" maxLength="2" className="bg-gold dib br3 pa3 ma2 grow bw2 shadow-5" id="4" onChange={this.handleInputChange}></input>
+                    <input type="text" maxLength="2" className="bg-orange dib br3 pa3 ma2 grow bw2 shadow-5" id="1" onChange={this.handleInputChange}></input>
+                    <input type="text" maxLength="2" className="bg-orange dib br3 pa3 ma2 grow bw2 shadow-5" id="2" onChange={this.handleInputChange}></input>
+                    <input type="text" maxLength="2" className="bg-orange dib br3 pa3 ma2 grow bw2 shadow-5" id="3" onChange={this.handleInputChange}></input>
+                    <input type="text" maxLength="2" className="bg-orange dib br3 pa3 ma2 grow bw2 shadow-5" id="4" onChange={this.handleInputChange}></input>
                 </form>
                 <br />
                 <br />
                 <div>
-                    <button className="bg-light-green dib br3 pa3 ma2 bw1 shadow-5 spin-button" variant="secondary" type="submit" onClick={this.checkAnswer}>Unlock</button>
+                    <button className="bg-orange dib br3 pa3 ma2 bw1 shadow-5 spin-button" variant="secondary" type="submit" onClick={this.checkAnswer}>Unlock</button>
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from 'Header';
-import Card from 'Card';
-import Footer from 'Footer';
+import Header from './Header'
+import Card from './Card';
+import Footer from './Footer';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
+
   <div>
     <div>
       <Header />
     </div>
-    <div>
+    <div className="tc pt7">
       <Card />
     </div>
     <div>
       <Footer />
     </div>
   </div>
-  ,document.getElementById('root')
+  , document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
